a8ab0ea6cd83c29817322b6060656b0dddc8db4a
Antonio Labinjan <alabinjan6@gmail.com>
2024-11-04 23:41:38 +0100

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Data from API</h1>
+    <p v-if="data">{{ data }}</p>
+    <p v-if="error">Error: {{ error }}</p>
+    <button @click="fetchData">Fetch Data</button>
+  </div>
+</template>
+
+<script>
+import { useExampleStore } from './stores/exampleStore';
+
+export default {
+  setup() {
+    const store = useExampleStore();
+
+    return {
+      data: store.data,
+      error: store.error,
+      fetchData: store.fetchData,
+    };
+  },
+};
+</script>
